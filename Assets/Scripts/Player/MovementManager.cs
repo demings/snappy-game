589d302f550e9315fb65b996b557d3dc0f3ec891
Mindaugas Laskis <mindaugas.laskis@gmail.com>
2021-07-16 20:54:26 +0300

    Implement jumping

--- Assets/Scripts/Player/MovementManager.cs
+++ Assets/Scripts/Player/MovementManager.cs
@@ -5,7 +5,11 @@ public class MovementManager : MonoBehaviour
 {
     Rigidbody2D rb;
     readonly float speed = 5f;
+    private const float maxYVelocity = 30;
+    private const float minYVelocity = -30;
     Vector2 vector;
+    public JumpState jumpState = JumpState.Grounded;
+    public Transform groundCheck;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +19,34 @@ public class MovementManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(vector.x * speed, rb.velocity.y);
+        var y = Mathf.Clamp(rb.velocity.y, minYVelocity, maxYVelocity);
+        rb.velocity = new Vector2(vector.x * speed, y);
+
+        if (y < 0)
+            jumpState = JumpState.Landing;
+
+        if (y == 0 && jumpState == JumpState.Landing)
+            jumpState = JumpState.Grounded;
+
     }
 
+    
     public void Move(InputAction.CallbackContext context) =>
         vector = context.ReadValue<Vector2>();
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.ReadValue<float>() > 0)
+        if (context.ReadValue<float>() > 0 && jumpState == JumpState.Grounded)
+        {
             rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+            jumpState = JumpState.Jumping;
+        }
     }
+}
+
+public enum JumpState
+{
+    Grounded,
+    Jumping,
+    Landing
 }
